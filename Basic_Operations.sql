bcf98e6672e43a5ccc5a7802645553851ee48c5c
Prerak <96664052+Prerak8880@users.noreply.github.com>
2024-10-10 08:35:00 +0530

    Create Basic_Operations.sql

---
+++ Basic_Operations.sql
@@ -0,0 +1,8 @@
+--Average of a Salary Column
+SELECT AVG(emp_sal) FROM Employees;
+--Sum of a Salary Column
+SELECT SUM(emp_sal) FROM Employees;
+--Minimum in a Column
+SELECT MIN(emp_sal) FROM Employees;
+--Maximum in a Column
+SELECT MAX(emp_sal) FROM Employees;
